4bf347399cb2d571338a11957d9146f670b3063e
Argatmj <ikomangarga0612@gmail.com>
2024-07-24 18:25:26 +0200

    Added add book function

--- script.js
+++ script.js
@@ -14,15 +14,13 @@ function Book(author, title, pages, read) {
 
 function addBook(book) {
   myLibrary.push(book);
-  let index = myLibrary.findIndex(libraryBook => book.title === libraryBook.title);
-  book.data = index;
 }
 
 function displayBooks() {
   const display = document.querySelector(".display");
   display.innerHTML = "";
 
-  myLibrary.forEach((book,index) => {
+  myLibrary.forEach((book, index) => {
     const bookDisplay = document.createElement("div");
     const author = document.createElement("div");
     const title = document.createElement("div");
@@ -43,23 +41,23 @@ function displayBooks() {
     read.innerHTML = book.read ? "Read" : "Not Read";
     remove.innerHTML = "Remove";
 
-    read.style.backgroundColor = book.read ? "#4ade80" : "#f87171";
+    read.style.backgroundColor = book.read ? "#86efac" : "#fca5a5";
 
     read.addEventListener("click", (e) => {
       let text = e.target.innerHTML;
       if (text === "Read") {
         e.target.innerHTML = "Not Read";
-        e.target.style.backgroundColor = "#f87171";
+        e.target.style.backgroundColor = "#fca5a5";
       } else {
         e.target.innerHTML = "Read";
-        e.target.style.backgroundColor = "#4ade80";
+        e.target.style.backgroundColor = "#86efac";
       }
     });
 
     remove.addEventListener("click", () => {
-        myLibrary.splice(index,1);
-        displayBooks();
-    })
+      myLibrary.splice(index, 1);
+      displayBooks();
+    });
 
     bookDisplay.appendChild(author);
     bookDisplay.appendChild(title);
@@ -71,9 +69,28 @@ function displayBooks() {
   });
 }
 
-const book1 = new Book("Kevin", "The Assassin", 123, true);
-const book2 = new Book("Bob", "The Assassin", 123, false);
-addBook(book1);
-addBook(book2);
+const newBook = document.querySelector(".new");
+const dialog = document.querySelector("dialog");
 
-displayBooks();
+newBook.addEventListener("click", () => dialog.showModal());
+
+const inputAuthor = document.querySelector("#author");
+const inputTitle = document.querySelector("#title");
+const inputPages = document.querySelector("#pages");
+const inputChecked = document.querySelector("#check");
+const submitBtn = document.querySelector("#submit");
+
+submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    dialog.close();
+
+    const author = inputAuthor.value;
+    const title = inputTitle.value;
+    const pages = inputPages.value;
+    const isChecked = inputChecked.checked;
+
+    let newBook = new Book(author, title, pages, isChecked);
+
+    addBook(newBook);
+    displayBooks();
+});
